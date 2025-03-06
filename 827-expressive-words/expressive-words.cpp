504fde53e05a42839cfca2c2d9bffe432b3c40ec
Shreya-Ragi <73057643+Shreya-Ragi@users.noreply.github.com>
2025-03-06 18:35:10 -0500

    Time: 3 ms (39.96%) | Memory: 12.1 MB (19.65%) - LeetSync

--- 827-expressive-words/expressive-words.cpp
+++ 827-expressive-words/expressive-words.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int count = 0;
-    void check_stretchy(vector<string>& groups, vector<string>& wordgroups){
+    void check_stretchy(vector<pair<char, int>>& groups, vector<pair<char, int>>& wordgroups){
         for(int i = 0; i<groups.size(); i++){
             // cout << groups[i] << " " << wordgroups[i] << ",";
-            if(groups[i].size() < 3){
-                if(groups[i] != wordgroups[i]){
+            if(groups[i].second < 3){
+                if(groups[i].second != wordgroups[i].second){
                     count--;
                     return;
                 }
             }
             else{
-                if(groups[i][0] != wordgroups[i][0] || wordgroups[i].size() > groups[i].size()){
+                if(groups[i].first != wordgroups[i].first || wordgroups[i].second > groups[i].second){
                     count--;
                     return;
                 }
@@ -22,29 +22,29 @@ public:
     int expressiveWords(string s, vector<string>& words) {
         count = words.size();
         char currc = s[0];
-        string sub = "";
-        vector<string> groups;
+        int sub = 0;
+        vector<pair<char, int>> groups;
         for(int i=0; i< s.size(); i++){
             if(s[i] != currc){
-                groups.push_back(sub);
-                sub = s[i];
+                groups.push_back({currc, sub});
+                sub = 1;
                 currc = s[i];
             } 
-            else sub += s[i];
-            if(i == s.size()-1) groups.push_back(sub);
+            else sub += 1;
+            if(i == s.size()-1) groups.push_back({s.back(), sub});
         }
         for(string word : words){
             char currc = word[0];
-            string sub = "";
-            vector<string> wordgroups;
+            int sub = 0;
+            vector<pair<char, int>> wordgroups;
             for(int i=0; i<word.size(); i++){
                 if(word[i] != currc){
-                    wordgroups.push_back(sub);
-                    sub = word[i];
+                    wordgroups.push_back({currc, sub});
+                    sub = 1;
                     currc = word[i];
                 } 
-                else sub += word[i];
-                if(i == word.size()-1) wordgroups.push_back(sub);
+                else sub += 1;
+                if(i == word.size()-1) wordgroups.push_back({word.back(), sub});
             }
             if(groups.size() != wordgroups.size()){
                 count--;
